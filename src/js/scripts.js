ec07983e0ceab365f252586e257ad47eff26e070
Benn King <bennkingy@gmail.com>
2018-12-09 17:38:06 +0000

    Homepage general updates

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,19 @@
 
   $(function () {
     // FastShell
+
+  
+    // Initialize Swiper 
+        var swiper = new Swiper('.swiper-container2', {
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            }
+        });
+
+
   });
 
 })(jQuery, window, document);
