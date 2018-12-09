877b050f7cd2e802b730c62f259db8491d33cb7b
Benn King <bennkingy@gmail.com>
2018-12-09 17:38:13 +0000

    Added bootstrap grid system

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -12,6 +12,19 @@
 
   $(function () {
     // FastShell
+
+  
+    // Initialize Swiper 
+        var swiper = new Swiper('.swiper-container2', {
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            }
+        });
+
+
   });
 
 })(jQuery, window, document);
